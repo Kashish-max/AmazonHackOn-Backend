d0ebc5fd250e14a0e32eed51af4cd065360961b2
cantDecideOne <kashish@untether.tech>
2022-08-01 01:07:35 +0530

    fix heroku config

--- hackOn/settings.py
+++ hackOn/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
 MEDIA_URL = '/media/' # Public URL at the browser
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
